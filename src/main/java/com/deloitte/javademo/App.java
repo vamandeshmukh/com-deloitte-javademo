4ea42f25461e9bb4c96db74a7f143e3eea18fa6d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-07-18 12:26:18 +0530

    encap

--- src/main/java/com/deloitte/javademo/App.java
+++ src/main/java/com/deloitte/javademo/App.java
@@ -1,13 +1,24 @@
 package com.deloitte.javademo;
 
+import com.deloitte.javademo.entity.Student;
+
 /**
- * Hello world!
+ * 
+ * @author Vaman Deshmukh
+ *         {@link} https://github.com/vamandeshmukh/com-deloitte-javademo
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Start");
+		
+		Student mohan = new Student();
+		mohan.marks = 99; // 92 
+		System.out.println(mohan.marks);
+		
+	
+	}
 }
